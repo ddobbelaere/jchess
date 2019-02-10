199a3185f8153c4c40b7c008413ef3be5365a952
Dieter Dobbelaere <dieter.dobbelaere@gmail.com>
2019-02-10 12:10:29 +0100

    Extended magic utils unit test

--- src/test/java/io/github/ddobbelaere/jchess/chess/MagicUtilsTest.java
+++ src/test/java/io/github/ddobbelaere/jchess/chess/MagicUtilsTest.java
@@ -17,6 +17,10 @@
  */
 package io.github.ddobbelaere.jchess.chess;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -26,6 +30,13 @@ import org.junit.jupiter.api.Test;
  */
 class MagicUtilsTest
 {
+	/**
+	 * Interface of an operator that yield an attack bitboard.
+	 */
+	private interface GetAttackBitboardOperator
+	{
+		long apply(int a, long b);
+	}
 
 	/**
 	 * Test static methods.
@@ -37,8 +48,78 @@ class MagicUtilsTest
 		MagicUtils magicUtils = new MagicUtils();
 
 		// Test static methods.
-		System.out.println(ChessBoard.getBitboardDebugString(MagicUtils.getRookAttackBitboard(0, 0)));
-		System.out.println(ChessBoard.getBitboardDebugString(MagicUtils.getBishopAttackBitboard(0, 0)));
+		final int[][] rookMovements = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+		final int[][] bishopMovements = new int[][] { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
+
+		testGetAttackBitboardOperator(rookMovements, (a, b) -> MagicUtils.getRookAttackBitboard(a, b));
+		testGetAttackBitboardOperator(bishopMovements, (a, b) -> MagicUtils.getBishopAttackBitboard(a, b));
+	}
+
+	/**
+	 * Test the MagicUtils method for getting the attack bitboard for a certain
+	 * sliding piece type (rook or bishop).
+	 *
+	 * @param pieceMovements    Array holding the possible piece movement
+	 *                          directions.
+	 * @param functionUnderTest Static MagicUtils function under test.
+	 */
+	private void testGetAttackBitboardOperator(int[][] pieceMovements, GetAttackBitboardOperator functionUnderTest)
+	{
+		// Instantiate random number generator.
+		Random rng = new Random();
+
+		// Number of random bitboards tested per square.
+		final int NUM_RANDOM_BB_PER_SQUARE = 1000;
+
+		for (int square = 0; square < 64; square++)
+		{
+			final int row = square / 8;
+			final int col = square % 8;
+
+			// Test with random bitboards of occupied squares.
+			for (int n = 0; n < NUM_RANDOM_BB_PER_SQUARE; n++)
+			{
+				// Generate random bitboard of occupied squares.
+				final long occupiedSquares = rng.nextLong();
+
+				// Calculate the attack bitboard in a straightforward (but slow) way.
+				long refAttackBitboard = 0;
+
+				for (final int[] pieceMovement : pieceMovements)
+				{
+					// Start at the piece square.
+					int attackedSquareRow = row;
+					int attackedSquareCol = col;
+
+					while (true)
+					{
+						attackedSquareRow += pieceMovement[0];
+						attackedSquareCol += pieceMovement[1];
+
+						if (attackedSquareRow < 0 || attackedSquareRow > 7 || attackedSquareCol < 0
+								|| attackedSquareCol > 7)
+						{
+							// This square falls outside the chess board.
+							break;
+						}
+
+						// Add the square to the attack bitboard.
+						refAttackBitboard |= ChessBoard.getSquareBitboard(attackedSquareRow, attackedSquareCol);
+
+						if ((ChessBoard.getSquareBitboard(attackedSquareRow, attackedSquareCol) & occupiedSquares) != 0)
+						{
+							// This square is occupied and hides all other following squares from the
+							// attack.
+							break;
+						}
+					}
+				}
+
+				// Verify that the result of the function under test matches the reference
+				// value.
+				assertEquals(functionUnderTest.apply(square, occupiedSquares), refAttackBitboard);
+			}
+		}
 	}
 
 }
